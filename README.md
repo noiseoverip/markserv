9c3d678326ad1d278d4fdf53f178a1fccafc6294
Saulius <alisauskas.saulius@gmail.com>
2020-12-30 15:03:23 +0000

    add systemd unit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # markserve
+
+go build main.go
+echo DIR=YOURDIR >> /etc/markser.env
+sudo cp markserv.service /etc/systemd/system/markserv.service
+sudo systemctl enable markserv
+sudo systemctl start markserv
+
--- main.go
+++ main.go
@@ -10,11 +10,12 @@ func main() {
 
   dir := flag.String("dir", "", "directory to serve")
   addr := flag.String("addr", ":3000", "IP addr to bind and port to liten on")
-
+  flag.Parse()
+   
   fs := http.FileServer(http.Dir(*dir))
   http.Handle("/", fs)
 
-  log.Printf("Listening on %s", addr)
+  log.Printf("Listening on %s", *addr)
   err := http.ListenAndServe(*addr, nil)
   if err != nil {
     log.Fatal(err)
